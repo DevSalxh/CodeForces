7b7ddf0e8fd51cdfdfe76e146ffb1e5c39ab0321
Saleh Mohammed Abu Saleh <devsalxh@gmail.com>
2024-07-31 08:24:44 +0300

    Add files via upload

---
+++ 050A-Domino.py
@@ -0,0 +1,5 @@
+from math import floor
+x,y=map(int,input().split())
+z = x*y
+result = int(floor(z/2))
+print(result)
---
+++ 110A-NearlyLucky.py
@@ -0,0 +1,14 @@
+n = input()
+
+counter = 0
+
+for digit in n:
+    if digit == '4' or digit == '7':
+        counter += 1
+
+if counter == 7 or counter == 4:
+    output = 'YES'
+else:
+    output = 'NO'
+
+print(output)
