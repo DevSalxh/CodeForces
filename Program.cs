08897c22379c93e6f9e15be162069f701f0f5422
Zen1x <devsalxh@gmail.com>
2026-07-21 18:48:40 +0300

    282A

--- Program.cs
+++ Program.cs
@@ -1,13 +1,4 @@
-﻿using var reader = new StreamReader(Console.OpenStandardInput());
+﻿using System.Data;
+using var reader = new StreamReader(Console.OpenStandardInput());
 using var writer = new StreamWriter(Console.OpenStandardOutput());
 
-int problemsNumber = int.Parse(reader.ReadLine()!);
-int acceptedSolution = 0;
-
-while (problemsNumber-- > 0)
-{
-    string solutionConfirmationMessage = reader.ReadLine()!;
-    acceptedSolution += solutionConfirmationMessage.Count(c => c == '1')>=2 ? 1 : 0;
-}
-
-writer.WriteLine(acceptedSolution);
